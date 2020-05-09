5b240a59a8e00dcdfb4947f2b71af06702ecefa3
lukeRosen <65053757+lukeRosen@users.noreply.github.com>
2020-05-08 19:37:59 -0700

    Add workerA and workerB for testing

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,11 @@
 import printColor from './workerUtilities.js';
 
-console.log("Hello!");
+console.log('Hello!');
 printColor('deepskyblue', 1000);
+
+console.warn('Beginning workerA.');
+Worker('workerScriptA.js');
+console.warn('Beginning workerB.');
+Worker('workerScriptB.js');
+
+console.warn('Main script complete.');
