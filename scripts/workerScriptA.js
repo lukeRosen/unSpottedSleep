ac21c5c6b513dc7df0ed7a37a6480e105db0f45f
lukeRosen <65053757+lukeRosen@users.noreply.github.com>
2020-05-08 19:49:31 -0700

    Update workerScriptA.js

--- scripts/workerScriptA.js
+++ scripts/workerScriptA.js
@@ -1,1 +1,7 @@
 printColor('hotpink', 1000);
+
+function printColor(color, lineCount){
+  for(let i = 0; i < lineCount; i++){
+    console.log(`%cLine number ${i}`, `background-color: ${color};`);
+  }
+}
