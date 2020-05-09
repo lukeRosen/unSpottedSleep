ba243ff0369b84ab2c7867430300453bb6b5d5ee
lukeRosen <65053757+lukeRosen@users.noreply.github.com>
2020-05-08 19:49:55 -0700

    Update workerScriptB.js

--- scripts/workerScriptB.js
+++ scripts/workerScriptB.js
@@ -1,3 +1,7 @@
-import printColor from './workerUtilities.js'
-
 printColor('deepskyblue', 1000);
+
+function printColor(color, lineCount){
+  for(let i = 0; i < lineCount; i++){
+    console.log(`%cLine number ${i}`, `background-color: ${color};`);
+  }
+}
