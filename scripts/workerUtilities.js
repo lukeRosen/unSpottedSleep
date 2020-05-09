3720d1a2668894dc60b854c158d74220851677df
lukeRosen <65053757+lukeRosen@users.noreply.github.com>
2020-05-08 19:22:44 -0700

    Add workerUtilities.js

---
+++ scripts/workerUtilities.js
@@ -0,0 +1,14 @@
+//3456789c123456789d123456789e123456789f123456789g123456789a123456789b123456789c
+//------------------------------------------------------------------------------
+
+//(string, number) => undefined
+//color: a CSS color as a string (or rgb/hsl/#)
+//lineCount: the number of lines to print out
+//prints 'lineCount' number of lines with 'color' background-color
+function printColors(color, lineCount){
+  for(let i = 0; i < lineCount; i++){
+    console.log(`%c$Line number ${i}`, `background-color: ${color};`);
+  }
+}
+
+export {printColors as default};
