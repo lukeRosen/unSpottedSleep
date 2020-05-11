c94b0d3aab21b1d52ab4a8e3f54d6187d92fa579
lukeRosen <65053757+lukeRosen@users.noreply.github.com>
2020-05-11 17:49:17 -0600

    Update mainApp.js

--- scripts/mainApp.js
+++ scripts/mainApp.js
@@ -1,4 +1,50 @@
 const implicitGrantResp = location.hash.substring(1).split('&');
 const accessToken = implicitGrantResp.find(str => str.indexOf('access_token') === 0).split('=')[1];
 
-console.log(accessToken);
+let uId;
+let postResp;
+
+(new Promise.resolve(getUserId(accessToken)))
+  .then(() => makeNewPlaylist(uId))
+  .finally(() => console.log('Completed.'));
+
+//will mutate/set uId in globalThis
+function getUserId(accessToken){
+  if(!accessToken){
+    console.warn('No access token found. Aborting getUserId()');
+    return;
+  }
+  
+  return fetch('https://api.spotify.com/v1/me', {
+    headers: {
+      Authorization: 'Bearer ' + accessToken
+    }})
+    .then(response => response.json())
+    .then(obj => {
+      arr = obj.uri.split(':');
+      uId = arr[arr.length - 1];})
+    .catch(err => console.error(`An error occured. Message was: ${err.message}`));
+}
+
+//will mutate/set postResp in globalThis
+function makeNewPlaylist(uId){
+  if(!uId){
+    console.warn('No user id found. Aborting makeNewPlaylist()');
+    return;
+  }
+  
+  return fetch(`https://api.spotify.com/v1/users/${uId}/playlists`, {
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + accessToken
+    },
+    body: JSON.stringify({
+      name: 'unSpottedSleep Playlist',
+      public: false,
+      description: 'Currently you\'ll probably want to delete this, or else it\'ll stack up.'
+    })
+  })
+    .then(response => response.json())
+    .then(obj => postResp = obj);
+    .catch(err => console.error(`An error occured in making a new playlist. Message was: ${err.message}`));
+}
