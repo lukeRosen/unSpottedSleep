a944059883572f4b7ffeda4f6330ef8476fd763a
lukeRosen <65053757+lukeRosen@users.noreply.github.com>
2020-05-16 17:32:45 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,1 @@
-import printColor from './workerUtilities.js';
-
-console.warn('Beginning workerA.');
-new Worker('scripts/workerScriptA.js');
-console.warn('Beginning workerB.');
-new Worker('scripts/workerScriptB.js');
-
-printColor('lime', 1000);
-
-console.warn('Main script complete.');
+console.log('Hello! Welcome to unSpottedSleep!');
