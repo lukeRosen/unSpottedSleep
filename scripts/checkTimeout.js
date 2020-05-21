dd620c4c67a8eb627a097775bb9f13e03cf51071
lukeRosen <65053757+lukeRosen@users.noreply.github.com>
2020-05-20 22:47:02 -0600

    Update checkTimeout.js

--- scripts/checkTimeout.js
+++ scripts/checkTimeout.js
@@ -1,6 +1,5 @@
-alert('Loaded Timeout Check!');
 const timeOutDuration = (1000 * 60) * 2; //currently set to 50 minutes for timeout
-const pollTime = (1000 * 60) * 1//currently set to poll every minute
+const pollTime = (1000 * 60) * (1 / 60)//currently set to poll every second
 
 let startTime = Date.now(); //when the webpage was loaded in ms from Unix Epoch
 
@@ -8,7 +7,6 @@ checkForTimeout(startTime, timeOutDuration, pollTime);
 
 //redirects user to login screen after timeout or rechecks after at least pollTime has elapsed
 function checkForTimeout(startTime, timeOutDuration, pollTime){
-  console.log('%cJust checked for timeout.', 'background-color: red;');
   if(timeOutDuration > (Date.now() - startTime)){
     setTimeout(checkForTimeout, pollTime, startTime, timeOutDuration, pollTime);
     return;
